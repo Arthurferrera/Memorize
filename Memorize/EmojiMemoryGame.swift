f3beb0e09193227688ac77b66bd4296a530340eb
Arthur Ferreira <arthur_f.oliveira@hotmail.com>
2020-08-02 22:10:15 -0300

    create viewModel - cardsView dynamic

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -9,10 +9,14 @@
 import SwiftUI
 
 class EmojiMemoryGame {
-    private var model: MemoryGame<String> =
-        MemoryGame<String>(numberOfPairsOfCards: 2, cardContentFactory: { (pairIndex: Int) -> String in
-            return "😀"
-        })
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻", "🎃", "🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
     
     // MARK: Access to the Model
     
