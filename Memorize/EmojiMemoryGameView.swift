3c329f7d07e5901c7fb5d07a19234cdd0ff64a01
Arthur Ferreira <arthur_f.oliveira@hotmail.com>
2020-08-14 01:49:56 -0300

    otimized code

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -22,7 +22,6 @@ struct EmojiMemoryGameView: View {
         }
         .padding()
         .foregroundColor(Color.orange)
-        .font(Font.largeTitle)
     }
 }
 
@@ -30,15 +29,29 @@ struct CardView: View {
     var card: MemoryGame<String>.Card
     
     var body: some View {
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
+        }
+    }
+    
+    func body(for size: CGSize) -> some View {
         ZStack {
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 20.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 20.0).stroke(lineWidth: 3)
+            if self.card.isFaceUp {
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth )
                 Text(card.content)
             } else {
-                RoundedRectangle(cornerRadius: 20.0).fill()
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
             }
         }
+        .font(Font.system(size: fontSize(for: size)))
+    }
+    
+//    MARK: - Drawing Constants
+    let cornerRadius: CGFloat = 15.0
+    let edgeLineWidth: CGFloat = 3
+    func fontSize(for size: CGSize) -> CGFloat {
+        min(size.width, size.height) * 0.75
     }
 }
 
